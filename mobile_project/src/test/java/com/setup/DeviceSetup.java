cbecdffac9b82eb429a3b9455d7500ac2f69276c
QAS-Spread <reiload@gmail.com>
2018-09-03 17:21:22 -0300

    Update DeviceSetup.java

--- mobile_project/src/test/java/com/setup/DeviceSetup.java
+++ mobile_project/src/test/java/com/setup/DeviceSetup.java
@@ -19,6 +19,28 @@ public class DeviceSetup extends BaseTest {
     //String device = System.getenv("device");
     //if(device != null && device.equalsIgnoreCase("Android")){
 
+    static AppiumDriver prepareMotoG5() throws MalformedURLException {
+
+        File currDir = new File("");
+        String path = currDir.getAbsolutePath();
+        System.out.println("Project Path: " + path);
+
+        //Set up desired capabilities and pass the Android app-activity and app-package to Appium
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("udid","0033378996");
+        capabilities.setCapability("deviceName","Moto_G__5_");
+        capabilities.setCapability("deviceVersion", "7.0");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("appPackage", "com.google.android.gm");
+        capabilities.setCapability("appActivity","com.google.android.gm.ConversationListActivityGmail"); // This is Launcher activity of your app (you can get it from apk info app)
+        File file = new File(path + "\\apk\\gmail-8-8-12-209077217-release.apk");
+        capabilities.setCapability("app", file.getAbsolutePath());
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().pageLoadTimeout( 10, TimeUnit.SECONDS );
+        return driver;
+    }
+
     static AppiumDriver prepareGalaxyA8() throws MalformedURLException {
 
         File currDir = new File("");
@@ -90,24 +112,24 @@ public class DeviceSetup extends BaseTest {
 
     // IOS
 
-       static AppiumDriver prepareDeviceIOS() throws MalformedURLException {
-            File appDir = new File("/application");
-            File app = new File(appDir, "foreksmobile.app");
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.2");
-            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS);
-            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"ahmet");
-            capabilities.setCapability("udid","82e1c906c4d00c16b24198035f0c2035d3d78ddf");
-            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-            capabilities.setCapability("appium-version", "1.7.2");
-            capabilities.setCapability("autoAcceptAlerts",false);
-            capabilities.setCapability("noReset","true");
-            capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
-            capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 30000);
-            driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            driver.manage().timeouts().pageLoadTimeout( 30, TimeUnit.SECONDS );
-            return driver;
-       }
+    static AppiumDriver prepareDeviceIOS() throws MalformedURLException {
+        File appDir = new File("/application");
+        File app = new File(appDir, "foreksmobile.app");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"ahmet");
+        capabilities.setCapability("udid","82e1c906c4d00c16b24198035f0c2035d3d78ddf");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        capabilities.setCapability("appium-version", "1.7.2");
+        capabilities.setCapability("autoAcceptAlerts",false);
+        capabilities.setCapability("noReset","true");
+        capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 30000);
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout( 30, TimeUnit.SECONDS );
+        return driver;
+    }
 }
 
